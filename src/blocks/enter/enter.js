eaa9c76d033f5aa9cf9c4fe8ba39fc37238f48eb
drcrowley <ksv.crowley@gmail.com>
2017-01-14 01:08:55 +0500

    fix enter forms

--- src/blocks/enter/enter.js
+++ src/blocks/enter/enter.js
@@ -1,42 +1,66 @@
 $(function () {
   var regForm = $('.js-reg-form'),
       authForm = $('.js-auth-form'),
-      apiUrl = 'http://94.142.139.199:8080';
+      html = $('html'),
+      apiUrl = 'http://94.142.139.199:8080/coupon-web/rs/',
+      cabinetUrl = 'http://yandex.ru';
 
-  regForm.parsley();
-  authForm.parsley();
+  if (regForm.length) {
+    regForm.parsley();
+  }
+  if (authForm.length) {
+    authForm.parsley();
+  }
 
   regForm.on('submit', function(event) {
     event.preventDefault();
     var form = $(this),
         formData = form.serializeArray(),
         regData = {
-          email: formData.email,
-          pwd: formData.pwd,
           news: false
         };
 
-    request('user/register', regData);
+    formData.forEach(function(item) {
+      regData[item.name] = item.value;
+    });
+
+    delete regData['reg-agree'];
+
+    request('user/register', regData, form);
   });
 
   authForm.on('submit', function(event) {
     event.preventDefault();
     var form = $(this),
-        formData = form.serializeArray();
+        formData = form.serializeArray(),
+        authData = {};
 
-    request('/user/login', formData);
+    formData.forEach(function(item) {
+      authData[item.name] = item.value;
+    });
+    request('user/login', authData, form);
   });
 
-  function request(url, data) {
+  function request(url, data, form) {
+    html.addClass('t-preloader');
+
     $.ajax({
-      type: "POST",
-      url: 'http://94.142.139.199:8080/' + url,
-      data: data,
+      type: 'POST',
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      url: apiUrl + url,
+      data: JSON.stringify(data),
       success: function(data) {
-        console.log('success', data);
+        if (data.status == 'OK') {
+          window.location.replace(cabinetUrl);
+        } else {
+          form.addClass('form_error');
+          html.removeClass('t-preloader');
+        }
       },
       error: function(data, message) {
         console.log(message);
+        html.removeClass('t-preloader');
       }
     });    
   }
