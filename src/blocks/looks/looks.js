a2f2137dd6acd4a76e929760f4c6ad92da4c7a47
drcrowley <ksv.crowley@gmail.com>
2017-03-19 19:15:35 +0500

    preview

---
+++ src/blocks/looks/looks.js
@@ -0,0 +1,11 @@
+$(function () {
+  var couponListScriptsUrl = typeof couponConfig != 'undefined' ? couponConfig.couponListScriptsUrl : '/coupon-list.js';
+
+  $('.js-looks-preview').on('click', function(event) {
+    event.preventDefault();
+    includeCoupons({colorTheme: 'blue', previewLink: '/show/couponsLanding'});
+  });
+
+  function includeCoupons(params){var request=new XMLHttpRequest;request.open("GET",couponListScriptsUrl,!0),request.onload=function(){request.status>=200&&request.status<400&&eval(request.responseText.replace(/{{params}}/g,JSON.stringify(params)))},request.send()}
+
+});
